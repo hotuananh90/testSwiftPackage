c0f69da7727e887e155f94ad96312e46cf0b52f1
hotuananh <hotuananh90@gmail.com>
2021-07-08 14:29:57 +0700

    public func

--- Sources/MyLibrary/TestSwiftPackage.swift
+++ Sources/MyLibrary/TestSwiftPackage.swift
@@ -1,12 +1,12 @@
 import XCTest
 
 public class TestSwiftPackage: XCTestCase {
-    let test = "test"
-    func testExample() {
+    public let test = "test"
+    public func testExample(text: String) {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MyLibrary().text, "Hello, World!")
+        XCTAssertEqual(text, "Hello")
     }
 
     static var allTests = [
