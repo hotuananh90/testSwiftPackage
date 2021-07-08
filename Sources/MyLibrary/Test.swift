54b6e04ef22cf1f10ba753b385f762ad70564964
hotuananh <hotuananh90@gmail.com>
2021-07-08 11:52:24 +0700

    init

---
+++ Sources/MyLibrary/Test.swift
@@ -0,0 +1,15 @@
+import XCTest
+
+class MyLibraryTests: XCTestCase {
+    let test = "test"
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MyLibrary().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
